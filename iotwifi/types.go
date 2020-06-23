8735bd25e361a95ebf6fe173c296e9d3cc9e2aa9
Steven Foerster <sfoerster@gmail.com>
2020-06-22 22:28:13 -0400

    types

--- iotwifi/types.go
+++ iotwifi/types.go
@@ -2,6 +2,7 @@ package iotwifi
 
 // SetupCfg is the main configuration structure.
 type SetupCfg struct {
+	WifiIface        string `json:"wifi_iface"`      // wifi_iface=wlan0
 	DnsmasqCfg       DnsmasqCfg       `json:"dnsmasq_cfg"`
 	HostApdCfg       HostApdCfg       `json:"host_apd_cfg"`
 	WpaSupplicantCfg WpaSupplicantCfg `json:"wpa_supplicant_cfg"`
@@ -11,6 +12,7 @@ type SetupCfg struct {
 type DnsmasqCfg struct {
 	Address     string `json:"address"`      // --address=/#/192.168.27.1",
 	DhcpRange   string `json:"dhcp_range"`   // "--dhcp-range=192.168.27.100,192.168.27.150,1h",
+	DhcpOption  string `json:"dhcp_option"`   // "--dhcp-option=6,10.2.3.1",
 	VendorClass string `json:"vendor_class"` // "--dhcp-vendorclass=set:device,IoT",
 }
 
